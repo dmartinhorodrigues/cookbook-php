4e734d4895ed0405c2adcdae05768ada1840ca0b
Artur Melo <artur.melo@beubi.com>
2015-08-14 15:07:01 +0100

    Add recipe 'module_intl' for installation of PHP INTL extension

---
+++ recipes/module_intl.rb
@@ -0,0 +1,31 @@
+#
+# Author:: Artur Melo (<artur.melo@beubi.com>)
+# Cookbook Name:: php
+# Recipe:: module_intl
+#
+# Copyright 2015, Ubiprism Lda.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+pkg = value_for_platform(
+  %w(centos redhat scientific fedora) => {
+    %w(5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.7 5.8) => "php53-intl",
+    "default" => "php-intl"
+  },
+  "default" => "php5-intl"
+)
+
+package pkg do
+  action :upgrade
+end
